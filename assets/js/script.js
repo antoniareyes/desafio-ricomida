ea86300cd85853702361209f15a2a73064f4a974
antoniareyes <reyanto98@gmail.com>
2020-11-20 11:25:43 -0300

    Primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function sendEmail(){
+   alert("Se envío la receta a tu correo!")
+}
+
+function addFavorite(){
+   alert("La receta se añadio a tus favoritos")
+}
+
+function empezar() {
+    alert("Esta función aún no esta disponible")
+}
